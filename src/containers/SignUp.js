9c29b352ec9c4af1e243c0c3ebfa05506e5fe54f
Lily Jen <lily@lilyjen.com>
2016-12-13 18:07:24 -0500

    adding form validation on sign up

---
+++ src/containers/SignUp.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react';
+import '../styles/SignUp.css'
+import classNames from 'classnames';
+
+export default class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pass: '',
+      confirmPass: '',
+      passwordTooShort: false,
+      passwordsDontMatch: false
+    };
+  }
+
+  setStateDone = () => {
+    console.log('current state', this.state);
+  }
+
+  checkPasswordLength = () => {
+    if (this.state.pass.length < 8) {
+      this.setState({
+        ...this.state,
+        passwordTooShort: true
+      });
+    } else {
+      this.setState({
+        ...this.state,
+        passwordTooShort: false
+      });
+    }
+  }
+
+  passwordMatchCheck = () => {
+    if (this.state.pass !== this.state.confirmPass) {
+      this.setState({
+        ...this.state,
+        passwordsDontMatch: true
+      });
+    } else {
+      this.setState({
+        ...this.state,
+        passwordsDontMatch: false
+      });
+    }
+  }
+
+  handleSubmit = () => {
+
+  }
+
+  render() {
+    const passwordClass = classNames({
+      'form-control': true,
+      'red-border': this.state.passwordTooShort
+    });
+    const passwordConfirmClass = classNames({
+      'form-control': true,
+      'red-border': this.state.passwordsDontMatch
+    });
+    const submitClass = classNames({
+      'btn': true,
+      'btn-default': true,
+    });
+    return (
+      <div className="sign-up">
+        <form name="sign-up-form">
+
+          <div className="form-group">
+            <label htmlFor="email">Email address</label>
+            <input type="email"
+                   className="form-control"
+                   id="email"
+                   placeholder="janedoe@gmail.com" />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="password"
+                   id="password"
+                   placeholder="8 character minimum"
+                   className={passwordClass}
+                   onChange={(e) => {
+                     this.setState(
+                       {...this.state, pass: e.target.value},
+                       this.setStateDone
+                     )}
+                   }
+                   onBlur={this.checkPasswordLength} />
+            { this.state.passwordTooShort ? <span>Password needs to be at least 8 characters long.</span> : null }
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="password-confirm">Confirm Password</label>
+            <input type="password"
+                   className={passwordConfirmClass}
+                   id="password-confirm"
+                   placeholder="Type it in again"
+                   onChange={(e) => {
+                     this.setState(
+                       {...this.state, confirmPass: e.target.value},
+                       this.setStateDone
+                     )}
+                   }
+                   onBlur={this.passwordMatchCheck}
+                   />
+                 { this.state.passwordsDontMatch ? <span>Your passwords dont match.. Try again?</span> : null }
+          </div>
+
+          <button type="submit" className={submitClass} onClick={this.handleSubmit}>Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,14 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import { Route, Router, IndexRoute, browserHistory} from 'react-router';
+import * as firebase from 'firebase';
 
 import App from './containers/App';
 import Home from './containers/Home';
+import SignUp from './containers/SignUp';
+
+const config = {
+  apiKey: "AIzaSyB7Rlj9IdHF3zBi-NSJlCYWX0GecMMWlXA",
+  authDomain: "jardin-822e1.firebaseapp.com",
+  databaseURL: "https://jardin-822e1.firebaseio.com",
+  storageBucket: "jardin-822e1.appspot.com",
+  messagingSenderId: "17435950385"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(
   <Router history={browserHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={Home} />
+      <Route path="/signup" component={SignUp} />
     </Route>
   </Router>,
   document.getElementById('root')
